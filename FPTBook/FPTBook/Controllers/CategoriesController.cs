61ab1ad9fe245b82689b98ddb6837d40a79d1717
bunpro1910 <103849197+bunpro1910@users.noreply.github.com>
2023-12-10 21:45:09 +0700

    v1

--- FPTBook/FPTBook/Controllers/CategoriesController.cs
+++ FPTBook/FPTBook/Controllers/CategoriesController.cs
@@ -89,8 +89,9 @@ namespace FPTBook.Controllers
 		// POST: Categories/Create
 		// To protect from overposting attacks, enable the specific properties you want to bind to.
 		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[Authorize("Administrator")]
-		[HttpPost]
+	
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
 		public async Task<IActionResult> Create([Bind("Id,Name,Description")] Category category)
         {
